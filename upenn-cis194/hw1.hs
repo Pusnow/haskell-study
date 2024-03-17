54228dfecb494f7368b63b35c7ae1fef6616d036
Wonsup Yoon <pusnow@kaist.ac.kr>
2024-03-17 16:49:39 +0900

    hw1-3

--- upenn-cis194/hw1.hs
+++ upenn-cis194/hw1.hs
@@ -23,6 +23,21 @@ doubleEveryOtherRev (x:y:xs) = [x, y*2] ++ doubleEveryOtherRev xs
 
 
 doubleEveryOther x = toRev(doubleEveryOtherRev (toRev x))
+
+
+-- EX3
+
+sumDigit :: Integer -> Integer
+sumDigit x 
+    | x `div` 10 == 0 = x
+    | otherwise = (x `mod` 10) + (sumDigit (x `div` 10))
+
+
+sumDigits :: [Integer] -> Integer
+sumDigits (x:xs) = sumDigit x + sumDigits xs
+sumDigits [] = 0
+
+
 -- MAIN
 
 main = do
@@ -32,6 +47,7 @@ main = do
     print("ex1-4", toDigits (-17) == [])
     print("ex2-1", doubleEveryOther [8,7,6,5] == [16,7,12,5])
     print("ex2-2", doubleEveryOther [1,2,3] == [1,4,3])
+    print("ex3-1", sumDigits [16,7,12,5] == 22)
 
 
 
